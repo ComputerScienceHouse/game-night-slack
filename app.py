d53365123a62323ae65ce4e22990647e9d5490f9
mfrancis95 <mikefrancis95@gmail.com>
2018-05-28 17:47:07 -0400

    Make app OpenShift-friendly

--- app.py
+++
@@ -1,4 +0,0 @@
-from game_night_slack import app
-
-if __name__ == '__main__':
-    app.run()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from game_night_slack import app as application
+
+if __name__ == '__main__':
+    application.run()
