909ab67ce53c588686ffb362ee70c9e8e33ff868
mfrancis95 <mikefrancis95@gmail.com>
2018-06-03 02:42:25 -0400

    Resolve #2

--- game_night_slack/commands.py
+++ game_night_slack/commands.py
@@ -17,12 +17,23 @@ def owner():
 
 
 def search():
-    name = request.form['text']
-    if name:
-        games = get(environ['GAME_NIGHT_URL'], auth = _auth, params = {'name': name}).json()
+    arguments = request.form['text'].split()
+    if arguments:
+        params = {}
+        if arguments[0] in ['-p', '--players']:
+            try:
+                params['players'] = int(arguments[1])
+                arguments = arguments[2:]
+            except:
+                return 'Usage: /gn-search [-p|--players] name'
+        params['name'] = ' '.join(arguments)
+        games = get(environ['GAME_NIGHT_URL'], auth = _auth, params = params).json()
         if len(games) == 0:
-            return 'We don\'t have any games that match "{}".'.format(name)
+            extra = ' and support {} player(s)'.format(params['players']) if 'players' in params else ''
+            return 'We don\'t have any games that match "{}"{}.'.format(params['name'], extra)
         elif len(games) == 1:
-            return 'We have 1 game that matches "{}" - *{}*.'.format(name, games[0]['name'])
-        return 'We have {} games that match "{}" - {}.'.format(len(games), name, ', '.join(map(lambda game: '*' + game['name'] + '*', games)))
-    return 'Usage: /gn-search name'
+            extra = ' and supports {} player(s)'.format(params['players']) if 'players' in params else ''
+            return 'We have 1 game that matches "{}"{} - *{}*.'.format(params['name'], extra, games[0]['name'])
+        extra = ' and support {} player(s)'.format(params['players']) if 'players' in params else ''
+        return 'We have {} games that match "{}"{} - {}.'.format(len(games), params['name'], extra, ', '.join(map(lambda game: '*' + game['name'] + '*', games)))
+    return 'Usage: /gn-search [-p|--players] name'
