06e4b5d8ebbcd8a80cfd1229a06601cd985563f0
mfrancis95 <mikefrancis95@gmail.com>
2018-08-14 13:08:48 -0400

    Resolve #5

--- game_night_slack/commands.py
+++ game_night_slack/commands.py
@@ -5,7 +5,8 @@ from requests import get
 from urllib.parse import urljoin
 from fuzzywuzzy.process import extractOne
 
-_game_mapper = lambda game: '*' + game['name'] + '*'
+_games = lambda games: ', '.join((f'*{game["name"]}*' for game in games))
+_parameters = lambda params: f'({", ".join((key + "=" + value for key, value in params.items()))})'
 _unreachable = 'Game Night is unreachable at the moment. Please try again later.', True
 
 _auth = GameNightAuth(environ['GAME_NIGHT_API_KEY'])
@@ -13,21 +14,17 @@ _auth = GameNightAuth(environ['GAME_NIGHT_API_KEY'])
 def newest():
     arguments = request.form.get('text', '').split()
     params = {}
-    if _parse_arguments(arguments, params) is None:
-        return 'Usage: /gn-newest [-p|--players]', True
+    if _parse_parameters(arguments, params) is None:
+        return _usage('newest', True)
     try:
         games = get(urljoin(environ['GAME_NIGHT_URL'], 'newest'), auth = _auth, params = params).json()
     except:
         return _unreachable
     if len(games) == 0:
-        return 'None of the newest games support {} player(s).'.format(params['players']), False
+        return f'None of the newest games match the parameters {_parameters(params)}.', False
     elif len(games) == 1:
-        try:
-            return 'One of the newest games that supports {} player(s) is *{}*.'.format(params['players'], games[0]['name']), False
-        except:
-            return 'The newest game is *{}*.'.format(games[0]['name']), False
-    extra = ' that support {} player(s)'.format(params['players']) if 'players' in params else ''
-    return 'The {} newest games{} are {}.'.format(len(games), extra, ', '.join(map(_game_mapper, games))), False
+        return f'The newest game that matches the parameters {_parameters(params)} is *{games[0]["name"]}*.', False
+    return f'The {len(games)} newest games that match the parameters {_parameters(params)} are {_games(games)}.', False
 
 def owner():
     name = request.form.get('text')
@@ -37,39 +34,52 @@ def owner():
         except:
             return _unreachable
         if games:
-            name = extractOne(name, map(lambda game: game['name'], games))[0]
-            game = next(filter(lambda game : game['name'] == name, games))
-            return '*{}* owns *{}*.'.format(game.get('owner', 'CSH'), game['name']), False
-        return 'No one owns "{}".'.format(name), False
-    return 'Usage: /gn-owner name', True
+            name = extractOne(name, (game['name'] for game in games))[0]
+            game = next((game for game in games if game['name'] == name))
+            return f'*{game.get("owner", "CSH")}* owns *{game["name"]}*.', False
+        return f'No one owns "{name}".', False
+    return _usage('owner', False, [('name', False)])
 
-def _parse_arguments(arguments, params):
-    if arguments and arguments[0] in ['-p', '--players']:
+def _parse_parameters(arguments, params):
+    while arguments and arguments[0] in ['-o', '--owner', '-p', '--players']:
         try:
-            params['players'] = int(arguments[1])
+            if '-o' in arguments[0]:
+                params['owner'] = arguments[1]
+            else:
+                params['players'] = arguments[1]
             arguments = arguments[2:]
         except:
             return None
     return arguments
 
 def search():
-    arguments = request.form.get('text', '').split()
+    params = {}
+    arguments = _parse_parameters(request.form.get('text', '').split(), params)
+    if arguments is None:
+        return _usage('search', True, [('name', True)])
     if arguments:
-        params = {}
-        arguments = _parse_arguments(arguments, params)
-        if arguments is None:
-            return 'Usage: /gn-search [-p|--players] name', True
         params['name'] = ' '.join(arguments)
-        try:
-            games = get(environ['GAME_NIGHT_URL'], auth = _auth, params = params).json()
-        except:
-            return _unreachable
-        if len(games) == 0:
-            extra = ' and support {} player(s)'.format(params['players']) if 'players' in params else ''
-            return 'We don\'t have any games that match "{}"{}.'.format(params['name'], extra), False
-        elif len(games) == 1:
-            extra = ' and supports {} player(s)'.format(params['players']) if 'players' in params else ''
-            return 'We have 1 game that matches "{}"{} - *{}*.'.format(params['name'], extra, games[0]['name']), False
-        extra = ' and support {} player(s)'.format(params['players']) if 'players' in params else ''
-        return 'We have {} games that match "{}"{} - {}.'.format(len(games), params['name'], extra, ', '.join(map(_game_mapper, games))), False
-    return 'Usage: /gn-search [-p|--players] name', True
+    elif not params:
+        return 'This query would return all games. Use some filters to narrow down the results.', True
+    try:
+        games = get(environ['GAME_NIGHT_URL'], auth = _auth, params = params).json()
+    except:
+        return _unreachable
+    if len(games) == 0:
+        return f'We don\'t have any games that match the parameters {_parameters(params)}.', False
+    elif len(games) == 1:
+        return f'We have 1 game that matches the parameters {_parameters(params)} - *{games[0]["name"]}*.', False
+    return f'We have {len(games)} games that match the parameters {_parameters(params)} - {_games(games)}.', False
+
+def _usage(command, params = False, arguments = []):
+    usage = f'Usage: `/gn-{command}`'
+    if params:
+        usage += ' [`OPTION`...]'
+    for argument, optional in arguments:
+        usage += f' [`{argument}`]' if optional else f' `{argument}`'
+    if params:
+        usage += '''\nWhere `OPTION` is one of:
+
+`-o`, `--owner`\t\tNarrow down the results by the specified game owner.
+`-p`, `--players`\tNarrow down the results by the supported number of players.'''
+    return usage, True
